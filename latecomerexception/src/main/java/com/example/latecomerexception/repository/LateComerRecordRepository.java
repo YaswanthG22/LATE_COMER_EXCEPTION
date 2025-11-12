3c8662f9207143a6f4530a357afa80149141aba1
YaswanthG22 <goliyaswanth9@gmail.com>
2025-11-12 13:20:32 +0530

    Delete latecomerexception directory

--- latecomerexception/src/main/java/com/example/latecomerexception/repository/LateComerRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.latecomerexception.repository;
-
-import com.example.latecomerexception.entity.LateComerRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LateComerRecordRepository extends JpaRepository<LateComerRecord, Long> {}
