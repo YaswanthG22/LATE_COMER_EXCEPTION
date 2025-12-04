82dbdcceff23abd2af328ed2f9976262d374d939
YaswanthG22 <goliyaswanth9@gmail.com>
2025-12-04 23:51:39 +0530

    Delete latecomerexception directory

--- latecomerexception/src/main/java/com/example/latecomerexception/repository/AuthorityDocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.latecomerexception.repository;
-
-import com.example.latecomerexception.entity.AuthorityDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityDocumentRepository extends JpaRepository<AuthorityDocument, Long> {
-}
--- latecomerexception/src/main/java/com/example/latecomerexception/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.latecomerexception.repository;
-
-import com.example.latecomerexception.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-}
